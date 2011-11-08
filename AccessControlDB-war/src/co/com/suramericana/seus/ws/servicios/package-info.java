9dcbdf2f7e8bc044bb43be05bcbb7ea8991784b2
SURAMERICANA\andrteca <SURAMERICANA\andrteca@f23d58e4-bce0-fc43-a206-869266b0538d>
2011-11-08 21:48:34 +0000

    Actualizacion
    git-svn-id: http://inmdecvd02.suranet.com:4747/svn/arquitectura/AccessControlDB/Principal/Codigo/trunk@7864 f23d58e4-bce0-fc43-a206-869266b0538d

--- AccessControlDB-war/src/co/com/suramericana/seus/ws/servicios/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://servicios.ws.seus.suramericana.com.co/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package co.com.suramericana.seus.ws.servicios;
