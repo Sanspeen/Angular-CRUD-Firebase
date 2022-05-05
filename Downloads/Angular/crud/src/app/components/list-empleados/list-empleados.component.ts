1c0fcb208cab7fcf396aef439600db070300c87a
Santiago Franco <santiago.franco@sofka.com.co>
2022-05-05 14:20:54 -0500

    Delete has been created

--- Downloads/Angular/crud/src/app/components/list-empleados/list-empleados.component.ts
+++ Downloads/Angular/crud/src/app/components/list-empleados/list-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-list-empleados',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-empleados.component.css']
 })
 export class ListEmpleadosComponent implements OnInit {
-  constructor() {
+  
+  empleados:any[] = [];
+  
+  constructor(private _empleadoService:EmpleadoService) {
    }
 
   ngOnInit(): void {
+    this.getEmpleados();
+  }
+
+  getEmpleados(){
+    this._empleadoService.getEmpleados().subscribe(data => {
+      this.empleados = [];
+      data.forEach((element:any) => {
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.empleados);
+    })
+  }
+
+  eliminar(id:string){
+    this._empleadoService.eliminarEmpleado(id).then(() => {
+      console.log('Eliminado correctamente');
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
 }
--- Downloads/Angular/crud/src/app/services/empleado.service.ts
+++ Downloads/Angular/crud/src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,13 @@ export class EmpleadoService {
 
   agregarEmpleado(empleado: any): Promise<any>{
     return this.firestore.collection('empleado').add(empleado)
+  }
+
+  getEmpleados(): Observable<any> {
+    return this.firestore.collection('empleado', res => res.orderBy('fechaCreacion', 'asc')).snapshotChanges();
+  }
+
+  eliminarEmpleado(id:string): Promise<any>{
+    return this.firestore.collection('empleado').doc(id).delete();
   }
 }
